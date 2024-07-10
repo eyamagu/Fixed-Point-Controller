38b07f82643dba50ee2f8d0bedf5a6133e4c24e7
eyamagu <152663204+eyamagu@users.noreply.github.com>
2024-07-10 14:15:40 -0400

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -1,7 +1,7 @@
 # if the argument is "all_tests" then run all the tests
 if [ "$1" == "panda_sweep" ]; then
     echo "Running all tests"
-    python3 scripts/whole_test.py --min-gravity-int 15 --max-gravity-int 15 --min-gravity-frac 15 --max-gravity-frac 15 --min-fd-int 8 --max-fd-int 10 --min-fd-frac 8 --max-fd-frac 10
+    python3 scripts/whole_test.py --min-gravity-int 15 --max-gravity-int 15 --min-gravity-frac 15 --max-gravity-frac 15 --min-fd-int 1 --max-fd-int 15 --min-fd-frac 1 --max-fd-frac 15
 elif [ "$1" == "panda" ]; then
     echo "Running panda trajectory tracking"
     make
